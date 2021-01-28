a8ad055bb042d6342a6627f64e74b4c3878546cb
Nafih-SA <nafih@softwareassociates.in>
2021-01-28 11:40:12 +0530

    Add AT base program

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # ESP32_SIM7600-GPS-GSM-Asset-Tracking
 **ESP32** is a well known WiFi integrated SoC. The **SIM7600EI** is a 4G/GSM/GPRS/GPS UART modem.
 </br>
+
+
 (to be updated)
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,66 @@
 #include <Arduino.h>
+#include <string.h>
+// #include <EEPROM.h>
 
-void setup() {
-  // put your setup code here, to run once:
+/********** Macros & Varaibles ******************/
+#define DEBUG 1
+bool stringComplete = false;
+String msg = "";
+
+/********* Function Definitions ****************/
+void AT_init()
+{
+#ifdef DEBUG
+  Serial.println("...AT Auto Baud rate set...");
+#endif
+  delay(500);
+  Serial2.println("AT");
+  delay(100);
+  Serial2.println("AT");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void serialEvent()
+{
+  while (Serial2.available())
+  {
+    // get the new byte:
+    char inChar = (char)Serial2.read();
+    // add it to the inputString:
+    msg += inChar;
+    // if the incoming character is a newline, set a flag so the main loop can
+    // do something about it:
+    if (inChar == '\n')
+    {
+      stringComplete = true;
+    }
+  }
+}
+
+/************ Setup ****************/
+void setup()
+{
+  Serial2.begin(115200);
+  Serial.begin(115200);
+  AT_init();
+}
+
+/************ Loop ****************/
+void loop()
+{
+  serialEvent();
+
+  /*********To Send AT Commands through Serial monitor**********/
+  if (Serial.available())
+  {                               // If anything comes in Serial (USB),
+    Serial2.write(Serial.read()); // read it and send it out Serial2
+  }
+
+  if (stringComplete)
+  {
+#ifdef DEBUG
+    Serial.println(msg);
+#endif
+    msg = "";
+    stringComplete = false;
+  }
 }
