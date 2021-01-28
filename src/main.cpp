7ea2cdfb64c11eb4916e5921f636b5b5dce9d92f
Nafih-SA <nafih@softwareassociates.in>
2021-01-28 16:14:33 +0530

    Added structure to save coordinates

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,14 @@
 #define DEBUG 1
 bool stringComplete = false;
 String msg = "";
+struct coords
+{
+  float Latitude = 0;
+  double Longitude = 0;
+  float speed = 0;
+  char *Hem;
+  char E_W;
+} data;
 
 /********* Function Definitions ****************/
 void AT_init()
@@ -61,6 +69,20 @@ void loop()
 #ifdef DEBUG
     Serial.println(msg);
 #endif
+
+    if (msg.indexOf("CGPSINFO") > 0)
+    {
+      char buf[512];
+      msg.toCharArray(buf, msg.length());
+      char *token = strtok(buf, ":");
+      token = strtok(NULL, ":");
+      data.Latitude = atof(strtok(token, ","));
+      data.Hem = strtok(NULL, ",");
+      data.Longitude = atof(strtok(NULL, ","));
+#ifdef DEBUG
+      Serial.printf("\t\t %s \nLatitude: %10.6f \nLongitude: %11.6f", data.Hem, data.Latitude, data.Longitude);
+#endif
+    }
     msg = "";
     stringComplete = false;
   }
